db0b362d4eca5d9f0e72de3d18a682815ff5a608
Amal Pradeep <amalpradeep12@gmail.com>
2024-10-22 11:50:45 +0530

    etst

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,33 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
-    >
+    <main className={`flex h-screen flex-col items-center ${inter.className}`}>
       <h1 className="text-5xl font-extralight text-gray-600 mt-10">Welcome!</h1>
-      <div className="mt-2">
+      <div className="m-2">
         <UserForm />
       </div>
+
+      <iframe
+        src="https://alpha.aftontickets.com/embeded/1"
+        id="aft-listing"
+        scrolling="no"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+        sandbox="allow-popups allow-forms allow-scripts allow-same-origin allow-downloads allow-top-navigation-by-user-activation"
+        style={{
+          border: 'none',
+          position: 'static',
+          visibility: 'visible',
+          display: 'inline-block',
+          width: '100%',
+          padding: '0px',
+          maxWidth: '100%',
+          minWidth: '180px',
+          marginTop: '0px',
+          marginBottom: '0px',
+        }}
+      ></iframe>
     </main>
   );
 }
